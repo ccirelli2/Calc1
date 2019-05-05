a298a4b04f1ff60a8f7aa318290628891818b7b2
Chris Cirelli <Chris.Cirelli@starrcompanies.com>
2019-05-04 20:10:34 -0400

    Commit

--- graph_poly_functions_05042019.R
+++ graph_poly_functions_05042019.R
@@ -30,8 +30,48 @@ p = ggplot() + geom_line(data = df, aes(x = X, y = df$p1, color = 'p1')) +
 p
 
 
-x = seq(-2, 2, .01)
-y = x^4 - 3*(x^2) + x
-plot(y)
 
+
+# Power Functions
+x = seq(-1,1, 0.1)
+y2 = function(x, p){
+  return(x^p)
+}
+
+y2.1 = y2(x, 1)
+plot(y2.1)
+
+y2.2 = y2(x, 2)
+plot(y2.2)
+
+y2.3 = y2(x, 3)
+plot(y2.3)
+
+y2.5 = y2(x,5)
+plot(y2.5)
+
+# Root Function
+x = seq(-10,10, 1)
+y3 = function(x, p){
+  return(x^(1/p))
+}
+y3.1 = y3(x, 1)
+plot(y3.1)
+
+y3.2 = y3(x, 2)
+plot(y3.2)
+
+y3.3 = y3(x, 3)
+plot(y3.3)
+
+
+# Trig functions
+X = seq(-10, 10, 1)
+g.x = cos(x)
+plot(x=X, y=g.x, 'l') 
+
+
+X = seq(-10, 10, 1)
+g.x = sin(x)
+plot(x=X, y=g.x, 'l') 
 
